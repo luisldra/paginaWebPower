6a4f305f7a488cc08046087aeada82dfeaf627eb
Luis David Rodriguez Arias <luisldra@gmail.com>
2023-04-14 18:54:59 -0500

    Implementacion de navegacion en todas las paginas, creacion de archivo JS

---
+++ programa.js
@@ -0,0 +1,22 @@
+
+
+function mostrarNavegacion() {
+    const estado = document.getElementById('navegacion').style.display
+    console.log(estado)
+    if (estado == 'block') {
+        document.getElementById('navegacion').style.display = 'none'      
+    }else{
+        document.getElementById('navegacion').style.display = 'block'
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navegacion = document.getElementById('btnNavegacion');
+    navegacion.addEventListener('click', mostrarNavegacion);
+
+
+
+})
+
